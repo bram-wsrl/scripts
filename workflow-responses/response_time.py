0b0ad84ec5374bd3274d7be007a5b1a55ce2eb12
bram-wsrl <b.berendsen@wsrl.nl>
2026-03-10 10:26:45 +0100

    workflow-response-times

---
+++ workflow-responses/response_time.py
@@ -0,0 +1,50 @@
+import time
+import datetime as dt
+import matplotlib.pyplot as plt
+
+
+def parse_duration(time_str):
+    time_str = time_str.strip()
+    if 'm' in time_str and 's' in time_str:
+        minutes, seconds = time_str.split('m')
+        minutes = int(minutes.strip())
+        seconds = int(seconds.strip().replace('s', ''))
+        return minutes * 60 + seconds
+    elif 'm' in time_str:
+        minutes = int(time_str.replace('m', '').strip())
+        return minutes * 60
+    elif 's' in time_str:
+        seconds = int(time_str.replace('s', '').strip())
+        return seconds
+    else:
+        raise ValueError(f"Invalid time format: {time_str}")
+
+
+with open('log-data/ImportOpvlWaterCGOO2h.txt', 'r') as file:
+    lines = file.readlines()
+
+# split line on tab
+times = []
+duration = []
+
+for line in lines:
+    parts = line.split('\t')
+    if len(parts) >= 2:
+        x, y = parts[1], parts[-1].strip('\n')
+        # parse this format: '05/03/2026 13:02:46 GMT'
+        x = x.replace(' GMT', '')
+        x = dt.datetime.strptime(x, '%d/%m/%Y %H:%M:%S')
+        times.append(x)
+        y = y.replace('WorkflowFiles/Imports/wf_ImportOpvlWaterCGOO2h.xml completed in ', '')
+        duration.append(parse_duration(y))
+
+fig, ax = plt.subplots()
+ax.plot(times, [d / 60 for d in duration], marker='o')
+# rotate x-axis labels for better readability
+plt.xticks(rotation=45)
+ax.set_xlabel('Time')
+ax.set_ylabel('Duration (minutes)')
+ax.set_title('wf_ImportOpvlWaterCGOO2h response times')
+ax.grid()
+plt.tight_layout()
+plt.savefig(f'figures/{int(time.time())}response_time.png')
